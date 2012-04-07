1dbcc8c9458f4f26dc3767bb111fae5fd4614f72
Israel Romero <israelalkarim@gmail.com>
2012-04-07 08:58:20 -0500

    Arreglado variable

--- tarea1-jruby.rb
+++ tarea1-jruby.rb
@@ -3,16 +3,28 @@
 class Tarea1 < Java::javax.swing.JFrame
 		def initialize(title)
 			super(title)
-		titulo1 = Java::javax.swing.JLabel.new('Una simple application')
+		titulo1 = Java::javax.swing.JLabel.new('Una simple application : ')
 		lblloginid = Java::javax.swing.JLabel.new('Login ID : ')
 		txtloginid = Java::javax.swing.JTextField.new('')
 		lblpassword = Java::javax.swing.JLabel.new('Password : ')
 		txtpassword = Java::javax.swing.JPasswordField.new('')
 		lblMessage = Java::javax.swing.JLabel.new('Message : ')
 		txtMessage = Java::javax.swing.JTextField.new('')
-		Enviarbtn = Java::java.awt.Button.new('Enviar') #error aqui?
-		Limpiarbtn = Java::java.awt.Button.new('Limpiar')
+		enviarbtn = Java::java.awt.Button.new('Enviar') #error aqui?
+		limpiarbtn = Java::java.awt.Button.new('Limpiar')
 		panel = Java::java.awt.Panel.new
+		panel.setLayout nil
+		enviarbtn.add_action_listener Enviarclick.new
+		titulo1.setBounds 20,20,220,30
+		lblloginid.setBounds 20,50,120,30
+		txtloginid.setBounds 170,50,120,30
+		lblpassword.setBounds 20,90,120,30
+		txtpassword.setBounds 170,100,120,30
+		lblMessage.setBounds 20,150,120,30
+		txtMessage.setBounds 170,150,120,30
+		enviarbtn.setBounds 60,200,60,40
+		limpiarbtn.setBounds 150,200,60,40
+		txtMessage.disable()
 		panel.add(titulo1)
 		panel.add(lblloginid)
 		panel.add(txtloginid)
@@ -20,11 +32,11 @@ class Tarea1 < Java::javax.swing.JFrame
 		panel.add(txtpassword)
 		panel.add(lblMessage)
 		panel.add(txtMessage)
-		panel.add(Enviarbtn)
-		panel.add(Limpiarbtn)
-		set_size (500,500)
+		panel.add(enviarbtn)
+		panel.add(limpiarbtn)
+		add(panel, Java::java.awt.BorderLayout::CENTER)
+		set_size(400,400)
 		set_visible(true)
-		Enviarbtn.add_action_listener Enviarclick.new
 	end
 end
 
@@ -33,7 +45,7 @@ class Enviarclick
 		include Java::Java.awt.event.ActionListener
 
 		def actionPerformed(e)
-			txtMessage.set "Registro completado"
+			txtMessage= "Registro completado"
 		end
 	end
 
